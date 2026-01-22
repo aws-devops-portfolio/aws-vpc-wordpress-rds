d6381d4df4b86ee5a954abdf3f2c753448f0da9b
MikeM164 <mikemonchu@gmail.com>
2026-01-22 23:01:25 +0200

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -10,11 +10,10 @@ terraform {
   required_version = ">= 1.2"
 
   backend "s3" {
-    bucket         = "devhub164-state-demo"
-    key            = "s3-github-actions/wordpress.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "devhub164-state-demo"
+    key          = "s3-github-actions/wordpress.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
 
