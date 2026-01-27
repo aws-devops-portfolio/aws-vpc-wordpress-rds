cddd8313fd3fd441ed0a46f4029f68ffcc5ff42d
MikeM164 <mikemonchu@gmail.com>
2026-01-27 11:07:50 +0200

    Update userdata.sh

--- terraform/modules/auto_scaling_group/userdata.sh
+++ terraform/modules/auto_scaling_group/userdata.sh
@@ -33,8 +33,8 @@ fi
 cat <<EOF >> /var/www/html/wp-config.php
 
 define('DB_NAME', '${DB_NAME}');
-define('DB_USER', '${DB_USER}');
-define('DB_PASSWORD', '${DB_PASSWORD}');
+define('DB_USER', '$${DB_USER}');
+define('DB_PASSWORD', '$${DB_PASSWORD}');
 define('DB_HOST', '${DB_HOST}');
 EOF
 
