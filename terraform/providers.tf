036d65c82fb24a20391f48e2e8208222a93ee971
MikeM164 <mikemonchu@gmail.com>
2026-05-14 01:12:42 +0200

    Add DynamoDB table and encryption to S3 backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,7 +13,8 @@ terraform {
     bucket         = "devhub164-state-demo"
     key            = "s3-github-actions/wordpress.tfstate"
     region         = "us-east-1"
-    use_lockfile   = true    
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true   
   }
 }
 
