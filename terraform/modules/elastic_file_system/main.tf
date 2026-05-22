c9753383c268ccf5c3d22a6eb2459ba82cc55b21
Mike <mikemonchu@gmail.com>
2026-05-22 17:56:38 +0200

    updated EFS terraform file

--- terraform/modules/elastic_file_system/main.tf
+++ terraform/modules/elastic_file_system/main.tf
@@ -1,3 +1,4 @@
+# Elastic File System (EFS) 
 resource "aws_efs_file_system" "wp_efs" {
   creation_token = "${var.prefix}-efs"
   encrypted      = true
@@ -7,6 +8,7 @@ resource "aws_efs_file_system" "wp_efs" {
   }
 }
 
+# EFS Mount Targets
 resource "aws_efs_mount_target" "wp_efs_mount_target" {
   for_each = toset(var.efs_subnet_ids)
 
